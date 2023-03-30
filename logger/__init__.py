ed2c773a9a6947aff97a28ab980b3cd1ece00a12
solfer <fvern.witherial01@tutanota.com>
2023-03-30 20:08:50 +0100

    Moved into /logger

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,9 +1,22 @@
+"""This module is to addon a more sophisticated
+external logging feature to the logging module.
+"""
+
+
 import os
 import logging
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
-def init_logging(log_name, log_folder:str='./logs', max_logs:int=10):
+
+def init_logging(
+        log_name, log_folder: str = './logs',
+        max_logs: int = 10, log_level=logging.DEBUG):
+    """Initializes the python logging function with the added feature
+    of an external folder with temporary log files, used for personal use.
+
+    :return: logging
+    """
     if not os.path.exists(log_folder):
         os.mkdir(log_folder)
     else:
@@ -16,14 +29,14 @@ def init_logging(log_name, log_folder:str='./logs', max_logs:int=10):
             logs_timed = {}
             for i in logs_in_dir:
                 logs_timed[i] = os.path.getmtime(f'{log_folder}/{i}')
-            for i in range(len(logs_in_dir)-max_logs):
+            for i in range(len(logs_in_dir) - max_logs):
                 oldest_log = min(logs_timed, key=logs_timed.get)
                 os.remove(
                     f'{log_folder}/{oldest_log}')
                 del logs_timed[oldest_log]
 
     log = logging.getLogger(log_name)
-    log.setLevel(logging.DEBUG)
+    log.setLevel(log_level)
 
     formatter = logging.Formatter(
         '[%(asctime)s]:%(levelname)s:%(name)s:%(message)s',
@@ -32,8 +45,9 @@ def init_logging(log_name, log_folder:str='./logs', max_logs:int=10):
     dt_string = datetime.now().strftime('%Y-%m-%d-%H%M%S')
     handler = RotatingFileHandler(
         f'logs/{dt_string}.log', mode='w',
-        maxBytes=5*1024*1024, backupCount=5, 
-        encoding=None, delay=0)
+        maxBytes=5 * 1024 * 1024, backupCount=5,
+        delay=False)
+
     handler.setFormatter(formatter)
     log.addHandler(handler)
 
