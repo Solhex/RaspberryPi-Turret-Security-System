490e94103106dc74705828f240c424892586a789
solfer <fvern.witherial01@tutanota.com>
2023-06-05 22:02:02 +0100

    Added additional variables with comments on what they do.

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
-email_receiver = 'RECEIVER EMAIL'
-email_addr = 'SENDER EMAIL'
-email_passwd = 'EMAIL PASSWORD'
+camera_id: int = 0
+# What camera will be scanning for object detection.
+
+capture_folder: str = './captures'
+# The folder where images will be stored
+
+email_receiver: str = 'turrettest@solfvern.com'
+email_addr: str = 'turretbuner@gmail.com'
+email_passwd: str = 'pwnawofdjasghpao'
+# What email will receive the security alerts and the credentials of the
+# sender email.
+
+turret_active: bool = False
+# Used to activate the turret, less laggy with it off (mainly for fun).
